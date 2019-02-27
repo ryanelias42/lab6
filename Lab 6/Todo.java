f210287c6469f8d56c2c47a5b963764a3f0bc925
Ryan E. <elias.ryan.matthew@gmail.com>
2019-02-26 23:06:58 -0600

    added todo to project

---
+++ Lab 6/Todo.java
@@ -0,0 +1,30 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-26
+OU ID_______________: 113481704
+OU EMAIL____________: ryanelias@ou.edu
+
+Tasks:
+1. complete shape classes
+2. create Rectangle class
+3. create square class
+4. complete comparator classes
+5. complete sorter test class
+6. complete shape test class
+
+Estimated Time (minutes):
+1. 60
+2. 30
+3. 10
+4. 45
+5. 45
+6. 60
+
+Actual Time (minutes):
+1. 60
+2. 10
+3. 10
+4. 45
+5. 70
+6. 90
+*/}
